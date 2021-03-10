317569e45b940d0f9c8ccc525b324a0224d62f6f
bKedds <48909880+bKedds@users.noreply.github.com>
2021-03-10 11:18:44 -0800

    Add files via upload

---
+++ guess-my-number/script.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let magicNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
+let highscore = 0;
+
+const displayMessage = message =>
+  (document.querySelector('.message').textContent = message);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // If no input
+  if (!guess) {
+    displayMessage('No number...');
+
+    //When player wins
+  } else if (guess === magicNumber) {
+    displayMessage('🎉 Correct Number!');
+    document.querySelector('.number').textContent = magicNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+
+    //Guess is wrong
+  } else if (guess !== magicNumber) {
+    if (score > 1) {
+      displayMessage(guess > magicNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      displayMessage('💥 You lost the game!');
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  magicNumber = Math.trunc(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = '';
+  document.querySelector('.score').textContent = score;
+  displayMessage('Start guessing...');
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('body').style.backgroundColor = '#222';
+});
